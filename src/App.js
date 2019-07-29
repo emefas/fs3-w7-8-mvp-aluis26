6b73b1fcf488e22328d3186e03ad417d18929ea2
Emefa Senoo <emefa.senoo@gmail.com>
2019-07-29 09:09:29 +0200

    Dashboard added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import DataContainer from './components/DataContainer';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+
 
 function App(){
     return(
         <div>
+            <BrowserRouter>
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/dashboard" component={Dashboard} />
             <DataContainer/>
+            </Switch>
+            </BrowserRouter>
         </div>
     )
 }
--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -1,66 +1,66 @@
-import React from 'react';
-import fetchData from '../Api';
-import DataDisplay from './DataDisplay';
+// import React from 'react';
+// import fetchData from '../Api';
+// import DataDisplay from './DataDisplay';
 
 
-class dataContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {            
-            clean:[]
+// class DataContainer extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {            
+//             clean:[]
           
            
-         }
-    }
+//          }
+//     }
 
-    componentDidMount() {
-        fetchData()
-          .then(data => {
-            console.log(data)
-            return data.items.map(row => {            
-                 //mapegem cada persona  
+//     componentDidMount() {
+//         fetchData()
+//           .then(data => {
+//             console.log(data)
+//             return data.items.map(row => {            
+//                  //mapegem cada persona  
     
-              const columns = row.answers.map(column => {
-                //   per cada persona dins de answers mapejem les 7 respostes i creem un nou nou objecte directament a return posant unes keys concretes amb els valors asignats.
-                if(column.type === "choice" ){
-                return {
-                  value: column.choice.label
-                };}
-                if(column.type === "choices" ){
-                  return{
-                    value: column.choices.labels
-                  }
-                }
-                else{ 
-                  return{
-                    value: column[column.type]
-                  }
-                }
+//               const columns = row.answers.map(column => {
+//                 //   per cada persona dins de answers mapejem les 7 respostes i creem un nou nou objecte directament a return posant unes keys concretes amb els valors asignats.
+//                 if(column.type === "choice" ){
+//                 return {
+//                   value: column.choice.label
+//                 };}
+//                 if(column.type === "choices" ){
+//                   return{
+//                     value: column.choices.labels
+//                   }
+//                 }
+//                 else{ 
+//                   return{
+//                     value: column[column.type]
+//                   }
+//                 }
     
-              });
-            // tanquem el map anterior (de cada persona) englobant les seves variables per cada una de les respostes amb la key column.
-              return {
-                columns
-              };
-            });
-          })
-          .then(data => {
-            console.log('TCL: dataContainer -> componentDidMount -> data', data);
-            this.setState({clean: data})
-            // ara ja tenim el format desitjat, tres arrays englobats en un object que contenen cada un nomes els valors que necessitem per la taula.
-          });
-    }
+//               });
+//             // tanquem el map anterior (de cada persona) englobant les seves variables per cada una de les respostes amb la key column.
+//               return {
+//                 columns
+//               };
+//             });
+//           })
+//           .then(data => {
+//             console.log('TCL: dataContainer -> componentDidMount -> data', data);
+//             this.setState({clean: data})
+//             // ara ja tenim el format desitjat, tres arrays englobats en un object que contenen cada un nomes els valors que necessitem per la taula.
+//           });
+//     }
 
 
-    render() {  
-      console.log(this.state.clean)
-         return ( 
-            <DataDisplay
-            clean = {this.state.clean}
-            />
-         );
-    }
-}
+//     render() {  
+//       console.log(this.state.clean)
+//          return ( 
+//             <DataDisplay
+//             clean = {this.state.clean}
+//             />
+//          );
+//     }
+// }
 
  
-export default dataContainer;
+// export default DataContainer;
