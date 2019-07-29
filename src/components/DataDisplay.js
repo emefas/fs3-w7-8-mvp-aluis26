dc733ca0220aa54a5ecb5b990e482bd657ddf534
Emefa Senoo <emefa.senoo@gmail.com>
2019-07-29 13:56:26 +0200

    Sticky table not working

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -1,37 +1,48 @@
 import React from 'react';
-import './DataDisplay.css'
+import './DataDisplay.css';
+import { StickyTable, Row, Cell } from 'react-sticky-table';
 
 
 function dataDisplay (props){
     console.log(props.clean)
     // var headers =["Name","Rating 1","Rating 2","Rating 3", "thoughts 1", "thoughts 2", "linkedin Profile"]
-    var headers = ["Name","Surname","Email","Email","Gender","ID","Birthday","Address","Nationality", "Second Nationality","Program Selected","Start Date Selected","CV Upload","LinkedIn","Motivation Note","Payment Options","Referral","17","18"]
+    var headers = ["Name","Surname","Email","Email","Gender","Phone Number","Birthday","Address","Nationality", "Second Nationality","Program Selected","Start Date Selected","CV Upload","LinkedIn","Motivation Note","Payment Options","Referral","17","18"];
+
+    // let rows = [];
+    // let cells;
+
+    // for (let r = 0; r < 50; r++){
+
+    // }
     return(
     <div>
       {/* <pre>
         <code>{JSON.stringify(props.clean, null, 4)}</code>
       </pre>   */}
       <h1>Data from typeform</h1>
-      <table>
-        <tr>
+      <div>
+      <StickyTable>
+        <Row>
         {headers.map(function (e,index){
-          return  <th key= {index}>{e}</th> } )}                  
-        </tr>
+          return  <Cell key= {index}>{e}</Cell> } )}                  
+        </Row>
       
         
-        {props.clean.map(function(e, index) {          
+        {props.clean.map(function(e, index) { 
+          console.log("e => ", e);         
            const rows = e.columns.map(function(val, index){
-            return <td key={index}>{val.value}</td>
+             console.log(val);
+            return <Cell key={index}>{val.value}</Cell>
           })
-          return <tr key={index}>{rows}</tr>
+          return <Row key={index}>{rows}</Row>
           
         })}
                      
         
-      </table>
+      </StickyTable>
 
 
-    
+      </div>
     </div>
 
     )
